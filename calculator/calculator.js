cf1bd1eb75e4b27ad5a8031ef98e7c384d3e5838
TechEdDan2 <dg.nelson2@gmail.com>
2024-05-27 17:33:30 -0400

    Finished updating functions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -46,9 +46,16 @@ function update() {
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  //Need to convert percent to decimal and months 
+  const monthlyRate = (values.rate / 100) / 12;
+  const monthlyPaymet = (values.amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -Math.floor(values.years * 12)));
+  // console.log(monthlyPaymet);
+  return monthlyPaymet;
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const paymentOutput = document.querySelector("#monthly-payment");
+  paymentOutput.innerHTML = "$" + monthly.toFixed(2);
 }
